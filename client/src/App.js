d819313683271c089244ad267ffd0b004bff0d5f
George-Kibe <georgekibewambui@gmail.com>
2023-02-05 16:20:25 +0300

    folder structure for frontend/client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+//import pages
+import Login from './pages/Login';
+import Password from './pages/Password';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Recover from './pages/Recover';
+import Reset from './pages/Reset';
+import PageNotFound from './pages/PageNotFound';
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Login/>
+  },
+  {
+    path:"/register",
+    element: <Register/>
+  },
+  // {
+  //   path:"/login",
+  //   element: <Login/>
+  // },
+  {
+    path:"/password",
+    element: <Password/>
+  },
+  {
+    path:"/profile",
+    element: <Profile/>
+  },
+  {
+    path:"/recover",
+    element: <Recover/>
+  },
+  {
+    path:"/reset",
+    element: <Reset/>
+  },
+  {
+    path:"*",
+    element: <PageNotFound/>
+  },
+])
+
+const App = () => {
   return (
-    <div className="App">
-      <p>React App</p>
-    </div>
-  );
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='container mx-auto'>
+        <div className='flex justify-center items-center h-screen'>
+            <div>
+                <div className='title'>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
